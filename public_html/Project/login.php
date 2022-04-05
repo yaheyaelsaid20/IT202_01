5ae15d6e86f33c04ea862a9e47aef9395f876307
YaheyaElsaid <yre@njit.edu>
2022-04-05 16:57:25 -0400

    login part 2

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -59,7 +59,9 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        echo "Welcome $email";
+                        echo "Weclome $email";
+                        $_SESSION["user"] = $user;
+                        die(header("Location: home.php"));
                     } else {
                         echo "Invalid password";
                     }
